3f9fde0f82cec29800fb980ca95ad79e237c2d34
Franco-Postulka <francopostulka@hotmail.com>
2024-10-22 21:28:54 -0300

    Mejora de la logica para obtenes mas productos

--- index.js
+++ index.js
@@ -15,48 +15,60 @@ document.addEventListener('DOMContentLoaded', function() {
             // navbarTitle.style.animationPlayState = 'paused'; 
         }
     });
+
     const contenedorRopa = document.getElementById("seccion-articulos");
+
+    const fetchCategory = async (category) => {
+        try {
+            const response = await fetch(`https://dummyjson.com/products/category/${category}?limit=20`);
+            console.log(response);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                return data.products;
+            } else {
+                console.error(`Error en el fetch de ${category}, status ${response.status}`);
+                return [];
+            }
+            } catch (error) {
+            console.error(`Error en el fetch de ${category}:`, error);
+            return [];
+            }
+        };
 
     const cargarRopa = async()=>{
         try{
-            const response = await fetch(`https://dummyjson.com/products/category/mens-shirts`);
-            const response2 = await fetch(`https://dummyjson.com/products/category/mens-shoes`);
+            const categories = ['mens-shirts', 'mens-shoes', 'womens-dresses', 'womens-shoes'];
+    
+            // se llamam a fetchCategory pasandole como parametro cada categoria
+            // devuelve un array de arrays (para cada categoria un array)
+            const productPromises = categories.map(fetchCategory);
+            const products = await Promise.all(productPromises);//se espera a la promesa y se guarda 
+            
+            // Convertimos el array de arrays en un array solo 
+            const combinedData = products.flat();
+            let productos = '';
 
-            // console.log(response.status);
-            if(response.status === 200 && response2.status ===200){
-                let data = await response.json();
-                let data2 = await response2.json();
-
-                data = data.products;
-                data2 = data2.products;
-
-                combinedData = [...data, ...data2];
-                console.log(combinedData);
-
-                
-                let productos = [];
-
-                combinedData.forEach(articulo => {
-                    productos += `
-                    <div class="articulo">
-                        <div>
-                            <a><img src="${articulo.images[0]}" alt=""></a>
-                        </div>
-                        <div>
-                            <h5>${articulo.title}</h5>
-                        </div>
-                        <div>
-                            <span>$ ${articulo.price}</span>
-                        </div>
-                        <div>
-                            <button type="button" class="btn btn-secondary">Agregar al carrito</button>
-                        </div>
+            combinedData.forEach(articulo => {
+                productos += `
+                <div class="articulo">
+                    <div>
+                        <a><img src="${articulo.images[0]}" alt=""></a>
                     </div>
-                    `;            
-                });
+                    <div>
+                        <h5>${articulo.title}</h5>
+                    </div>
+                    <div>
+                        <span>$ ${articulo.price}</span>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-secondary">Agregar al carrito</button>
+                    </div>
+                </div>
+                `;            
+            });
 
-                contenedorRopa.innerHTML = productos; 
-            }
+            contenedorRopa.innerHTML = productos; 
         }
     
         catch (error){
