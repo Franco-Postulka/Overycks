2029e12751ccd7dcc55413ec58d107bb275998e9
Lyrics-21 <elianviana8gmail.com>
2024-12-06 16:47:23 -0300

    Correcciones admin y eliminar favoritos

---
+++ static/admin.js
@@ -0,0 +1,7 @@
+const agregar = document.getElementById("agregar");
+const administrarProductos = document.getElementById("agregar");
+
+agregar.addEventListener("click", () =>
+{
+    agregar.style.display == "block";
+});
